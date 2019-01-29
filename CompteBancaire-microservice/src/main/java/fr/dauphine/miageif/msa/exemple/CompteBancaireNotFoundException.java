51b4d4a1a6bd59bb3f2560af269341aa8fb71488
huyanwing <huyanwing@macbook-pro-de-huyanwing.home>
2019-01-29 01:47:27 +0100

    First Commit Uploading

---
+++ CompteBancaire-microservice/src/main/java/fr/dauphine/miageif/msa/exemple/CompteBancaireNotFoundException.java
@@ -0,0 +1,17 @@
+package fr.dauphine.miageif.msa.exemple;
+
+public class CompteBancaireNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String DEFAULT_MESSAGE = "Compte bancaire n'est pas trouvé";
+
+    public CompteBancaireNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public CompteBancaireNotFoundException(String message) {
+        super(message);
+    }
+	
+} 
---
+++ CompteBancaire-microservice/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into compte_bancaire(id, iban, type_compte, interet, fraistenue, solde) values (1,'FR7630004000031234567890143','compte courant','0.0','0.0','100.0');
+insert into compte_bancaire(id, iban, type_compte, interet, fraistenue, solde) values (2,'FR7630004000031234567893333','compte courant','0.0','0.0','300.0');
+
+
